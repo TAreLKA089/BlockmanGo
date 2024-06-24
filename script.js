2320f5043a3df9864c815dda9bd8633c1bc44aef
TAreLKA089 <96261189+TAreLKA089@users.noreply.github.com>
2024-06-24 11:03:03 +0300

    W
    W

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
     const sidebar = document.getElementById('sidebar');
     const content = document.getElementById('content');
+    const h1 = document.getElementById('shiftBack');
     const closeBtn = document.getElementById('close-btn');
     const openBtn = document.getElementById('open-btn');
 
     closeBtn.addEventListener('click', function () {
         sidebar.classList.add('closed');
         content.classList.add('shifted');
+        h1.classList.add('shifted1');
+        h1.classList.remove('shifted2');
         openBtn.classList.add('show');
     });
 
     openBtn.addEventListener('click', function () {
         sidebar.classList.remove('closed');
         content.classList.remove('shifted');
+        h1.classList.remove('shifted1');
+        h1.classList.add('shifted2');
         openBtn.classList.remove('show');
     });
 });
